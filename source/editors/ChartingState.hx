8e564fe21157debe7331c70fc68c81fe3167f2db
LOL <89255771+Sksnksnskakwkw@users.noreply.github.com>
2022-01-03 13:34:34 +0800

    Idk

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -62,6 +62,8 @@ class ChartingState extends MusicBeatState
 		'Hurt Note',
 		'GF Sing',
 		'No Animation'
+                'Missle'
+                'Aflac Attack'
 	];
 	private var noteTypeIntMap:Map<Int, String> = new Map<Int, String>();
 	private var noteTypeMap:Map<String, Null<Int>> = new Map<String, Null<Int>>();
