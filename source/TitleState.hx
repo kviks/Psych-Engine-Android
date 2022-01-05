68d58af7beb56850065116e4af8f62749a1a27a0
Meditor01 <96972975+Meditor01@users.noreply.github.com>
2022-01-06 01:54:17 +0800

    N

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -466,11 +466,11 @@ class TitleState extends MusicBeatState
 			switch (sickBeats)
 			{
 				case 1:
-					createCoolText(['Psych Engine by'], 45);
+					createCoolText(['made by'], 45);
 				// credTextShit.visible = true;
 				case 3:
-					addMoreText('Shadow Mario', 45);
-					addMoreText('RiverOaken', 45);
+					addMoreText('m editor', 45);
+					addMoreText('yay', 45);
 				// credTextShit.text += '\npresent...';
 				// credTextShit.addText();
 				case 4:
@@ -479,9 +479,9 @@ class TitleState extends MusicBeatState
 				// credTextShit.text = 'In association \nwith';
 				// credTextShit.screenCenter();
 				case 5:
-					createCoolText(['This is a mod to'], -60);
+					createCoolText(['cool'], -60);
 				case 7:
-					addMoreText('This game right below lol', -60);
+					addMoreText('just for mhb', -60);
 					logoSpr.visible = true;
 				// credTextShit.text += '\nNewgrounds';
 				case 8:
