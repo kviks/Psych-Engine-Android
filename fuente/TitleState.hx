ef91388592448d06d246e65a6d20c5af0215421b
ChoripanUwU <96887699+ChoripanUwU@users.noreply.github.com>
2022-01-14 00:26:51 -0300

    TitleState.hx
    Es para mí mod/id for my mod :) :+1:

--- fuente/TitleState.hx
+++ fuente/TitleState.hx
@@ -471,45 +471,44 @@ class TitleState extends MusicBeatState
 				case 3:
 					addMoreText('Shadow Mario', 45);
 					addMoreText('RiverOaken', 45);
-				// credTextShit.text += '\npresent...';
 				// credTextShit.addText();
 				case 4:
 					deleteCoolText();
 				// credTextShit.visible = false;
-				// credTextShit.text = 'In association \nwith';
+				// credTextShit.text = 'Este mod \nEcho por';
 				// credTextShit.screenCenter();
 				case 5:
-					createCoolText(['This is a mod to'], -60);
+					createCoolText(['Este tipo'], -60);
 				case 7:
-					addMoreText('This game right below lol', -60);
+					addMoreText('xd', -60);
 					logoSpr.visible = true;
-				// credTextShit.text += '\nNewgrounds';
+				// credTextShit.text += '\nSuS';
 				case 8:
 					deleteCoolText();
 					logoSpr.visible = false;
 				// credTextShit.visible = false;
 
-				// credTextShit.text = 'Shoutouts Tom Fulp';
+				// credTextShit.text = 'si me funan algunas vez';
 				// credTextShit.screenCenter();
 				case 9:
 					createCoolText([curWacky[0]]);
 				// credTextShit.visible = true;
 				case 11:
 					addMoreText(curWacky[1]);
-				// credTextShit.text += '\nlmao';
+				// credTextShit.text += '\nhaslo no me importa';
 				case 12:
 					deleteCoolText();
 				// credTextShit.visible = false;
-				// credTextShit.text = "Friday";
+				// credTextShit.text = "Vs";
 				// credTextShit.screenCenter();
 				case 13:
-					addMoreText('Friday');
+					addMoreText('Fem');
 				// credTextShit.visible = true;
 				case 14:
 					addMoreText('Night');
-				// credTextShit.text += '\nNight';
+				// credTextShit.text += '\nBOY';
 				case 15:
-					addMoreText('Funkin'); // credTextShit.text += '\nFunkin';
+					addMoreText('Funkin'); // credTextShit.text += '\nXD';
 
 				case 16:
 					skipIntro();
